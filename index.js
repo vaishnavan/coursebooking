72ec6050a3a5b47d48d2126d0e815e4641773096
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2022-02-05 19:23:05 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const bookingRouter = require('./routes/booking.routes');
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config();
